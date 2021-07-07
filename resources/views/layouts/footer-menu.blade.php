0f3a7e5ef00b7539e9bc30c2dbac968dc0b573a9
ngochuong2049 <66053080+ngochuong2049@users.noreply.github.com>
2021-07-07 17:43:03 +0700

    update code

--- resources/views/layouts/footer-menu.blade.php
+++ resources/views/layouts/footer-menu.blade.php
@@ -1,55 +1,56 @@
 <!-- Navbar -->
-            <ul class="menu-mobile list-style-none">
-                <li class="menu-child">
-                    {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[0]['alias']]) }} ">
-                        {{ $productOfDistricts[0]['level']." ".$productOfDistricts[0]['name_local'] }}
-                    </a> --}}
-                    <a href="{{ route('productsOfCity',['city' => 'ho-chi-minh']) }} ">
-                        Khu vực Hồ Chí Minh
-                    </a>
-                </li>
-                <li class="menu-child">
-                    {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[1]['alias']]) }}">
-                        {{ $productOfDistricts[1]['level']." ".$productOfDistricts[1]['name_local'] }}
-                    </a> --}}
-                    <a href="{{ route('productsOfCity',['city' => 'binh-duong']) }} ">
-                        Khu vực Bình Dương
-                    </a>
-                </li>
-                <li class="menu-child">
-                    {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[2]['alias']]) }}">
-                        {{ $productOfDistricts[2]['level']." ".$productOfDistricts[2]['name_local'] }}
-                    </a> --}}
-                    <a href="{{ route('productsOfCity',['city' => 'dong-nai']) }} ">
-                        Khu vực Đồng Nai
-                    </a>
-                </li>
-                {{-- <li class="menu-child">
-                    <a href="{{ route('productsOfCity',['city' => 'khac']) }} ">
-                        Khu vực Khác
-                    </a>
-                </li> --}}
+<ul class="menu-mobile list-style-none">
+    <li class="menu-child">
+        {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[0]['alias']]) }} ">
+                {{ $productOfDistricts[0]['level']." ".$productOfDistricts[0]['name_local'] }}
+            </a> --}}
+        <a href="{{ route('productsOfCity', ['city' => 'ho-chi-minh']) }} " style="color: white !important">
+            Khu vực Hồ Chí Minh
+        </a>
+    </li>
+    <li class="menu-child">
+        {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[1]['alias']]) }}">
+                {{ $productOfDistricts[1]['level']." ".$productOfDistricts[1]['name_local'] }}
+            </a> --}}
+        <a href="{{ route('productsOfCity', ['city' => 'binh-duong']) }} " style="color: white !important">
+            Khu vực Bình Dương
+        </a>
+    </li>
+    <li class="menu-child">
+        {{-- <a href="{{ route('productsOfDistrict',['district' => $productOfDistricts[2]['alias']]) }}">
+                {{ $productOfDistricts[2]['level']." ".$productOfDistricts[2]['name_local'] }}
+            </a> --}}
+        <a href="{{ route('productsOfCity', ['city' => 'dong-nai']) }} " style="color: white !important">
+            Khu vực Đồng Nai
+        </a>
+    </li>
+    {{-- <li class="menu-child">
+            <a href="{{ route('productsOfCity',['city' => 'khac']) }} ">
+                Khu vực Khác
+            </a>
+        </li> --}}
 
-            </ul>
+</ul>
+
 <!-- End Navbar -->
 <script>
     window.addEventListener('DOMContentLoaded', () => {
-        $(".button-open-menu-mobile").on('click', function(){
+        $(".button-open-menu-mobile").on('click', function() {
             $(".menu-mobile-background").show();
             $(".menu-mobile-area").addClass('open');
 
             $(".menu-mobile-background").animate({
-                'opacity' : '1'
+                'opacity': '1'
             }, 300);
         });
-        $(".menu-mobile-close, .menu-mobile-background").on('click', function(){
+        $(".menu-mobile-close, .menu-mobile-background").on('click', function() {
             $(".menu-mobile-area").removeClass('open');
 
             $(".menu-mobile-background").animate({
-                'opacity' : '0'
+                'opacity': '0'
             }, 300);
 
-            setTimeout(function(){
+            setTimeout(function() {
                 $(".menu-mobile-background").hide();
             }, 300);
         });
--- resources/views/layouts/footer-static.blade.php
+++ resources/views/layouts/footer-static.blade.php
@@ -1,54 +1,55 @@
 <footer class="footer-static" id="footer-static">
-  <div class="container">
-    <div class="row footer-top">
-      <div class="footer-col-1 col-xs-12 col-md-5">
-        {{-- <h4 class="footer-title">thuechungcuhn.me</h4> --}}
-        <a href="">
-        <img src="{{asset('assets/img/Logo (2).png')}}" alt="">
-        </a>
-        <ul>
-          <li>
-            <i class="fa fa-envelope"></i> Email: fmi.jsc@gmail.com
-          </li>
-          <li>
-            <i class="fa fa-bolt"></i>  Hotline: 091 314 45 99
-          </li>
-           <li>
-            <i class="fa fa-map-marker"></i> Address: 36A Nguyễn Gia Trí, Phường 25, Quận Bình Thành, Tp.Hồ Chí Minh.
-          </li>
-        </ul>
-      </div>
+    <div class="container">
+        <div class="row footer-top">
+            <div class="footer-col-1 col-xs-12 col-md-5">
+                {{-- <h4 class="footer-title">thuechungcuhn.me</h4> --}}
+                <a href="">
+                    <img src="{{ asset('assets/img/Logo (2).png') }}" alt="">
+                </a>
+                <ul>
+                    <li>
+                        <i class="fa fa-envelope"></i> Email: fmi.jsc@gmail.com
+                    </li>
+                    <li>
+                        <i class="fa fa-bolt"></i> Hotline: 091 314 45 99
+                    </li>
+                    <li>
+                        <i class="fa fa-map-marker"></i> Address: 36A Nguyễn Gia Trí, Phường 25, Quận Bình Thành, Tp.Hồ
+                        Chí Minh.
+                    </li>
+                </ul>
+            </div>
 
-      <div class="footer-col-2 col-xs-12 col-md-7">
-        <div clas="row">
-            <div clas="col-xs-12 col-md-6">
-               <div class="footer-block">
+            <div class="footer-col-3 col-xs-12 col-md-7">
+                <div clas="row">
+                    <div clas="col-xs-12 col-md-6">
+                        <div class="footer-block">
 
-              </div>
-          </div>
+                        </div>
+                    </div>
 
-          <div clas="col-xs-12 col-md-6">
-            <div class="footer-block">
-          <h4 class="footer-title">Bất động sản theo khu vực</h4>
-          @include('layouts.footer-menu')
+                    <div clas="col-xs-12 col-md-6">
+                        <div class="footer-block">
+                            <h4 class="footer-title">Bất động sản theo khu vực</h4>
+                            @include('layouts.footer-menu')
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    <div class="footer-bottom">
-      <div class="row">
-        <div class="col-md-4 col-12 coppyright">
-          <i class="fas fa-copyright"></i><span>Bản quyền © 2020 thuộc về spaceland.vn</span>
-        </div>
-        <div class="col-md-8 col-12">
-          {{-- <ul class="social-contact ">
+        <div class="footer-bottom">
+            <div class="row">
+                <div class="col-md-4 col-12 coppyright">
+                    <i class="fas fa-copyright"></i><span>Bản quyền © 2020 thuộc về spaceland.vn</span>
+                </div>
+                <div class="col-md-8 col-12">
+                    {{-- <ul class="social-contact ">
             <li><a href=""><i class="fab fa-facebook"></i></a></li>
             <li><a href=""><i class="fab fa-youtube"></i></a></li>
             <li><a href=""><i class="fas fa-envelope"></i></a></li>
           </ul> --}}
+                </div>
+            </div>
         </div>
-      </div>
     </div>
-  </div>
 </footer>
